8c599b2d12a07eeadc23be29845c527e3ef93e88
milfoyz <tsfitoff@gmail.com>
2026-06-06 00:01:43 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,34 @@
+TOKEN = "8902526743:AAEsd0KAoHM4KMA2tyUeLoPfo_N4V4GnZRE"
+
+admins_id = [8207246901]
+
+SUBGRAM_TOKEN = "8ad969e0cdf4fcc82a48301d678ac9001afc0fbb11bb28966f0457155eca9344" 
+HIVIEWS_TOKEN = "8934BDN9327V3PB1JDVXB"
+FLYER_TOKEN = ''
+
+
+admin_username = "@temik_gaz" #свой username
+admin_link = "https://t.me/temik_gaz"
+
+GIFT_AMOUNT = 1.0 # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 160 # не трогать
+
+#основной канал
+channel_osn = "https://t.me/casseto"
+#чат
+chater = "https://t.me/munmab"
+id_chat = -1003586649297
+#канал выплат
+channel_viplat = "https://t.me/BorutoGift"
+channel_viplat_id = -1003961561317
+#канал выигрышей мини-игры:
+channel_link = "https://t.me/munmab"
+id_channel_game = -1003586649297
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
